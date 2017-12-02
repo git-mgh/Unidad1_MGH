84c30235226a11ff774d3d10026b4d77365e1d80
mauricio.giraldo <mauricio.giraldo@PROYECTOS-MG.LINEADATASCAN.local>
2017-12-02 10:05:00 -0500

    Carga inicial

---
+++ Unidad1-prueba1/src/com/lds/java/ClaseInicial.java
@@ -0,0 +1,11 @@
+package com.lds.java;
+
+public class ClaseInicial {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hola mundo!!!");
+
+	}
+
+}
